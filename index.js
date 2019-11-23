49b10ccb367eea83937c142eebfcb5f4893e54ad
unknown <ahedqaood19@gmail.com>
2019-11-23 04:20:44 +0300

    configure heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router     = require('./routes.js');
 const bodyParser = require('body-parser');
 const io         = require('socket.io')(server);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static('static'));
 app.use(bodyParser.json());
