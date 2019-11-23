12658d0cdb553a23f6760d4cbf60eb0d5cfbcf6e
unknown <ahedqaood19@gmail.com>
2019-11-23 15:08:09 +0300

    undo changes

--- routes.js
+++ routes.js
@@ -1,19 +1,10 @@
 const router = require('express').Router();
 
-router.post('/parking1_lot_status', (req, res) => {
+router.post('/parking_lot_status', (req, res) => {
         let io = req.app.get('socketio');
         let parkingInfo = req.body;
         
-        io.emit("parking1:info", parkingInfo);
-        console.log("Request received!");
-        res.sendStatus(200);
-});
-
-router.post('/parking2_lot_status', (req, res) => {
-        let io = req.app.get('socketio');
-        let parkingInfo = req.body;
-        
-        io.emit("parking2:info", parkingInfo);
+        io.emit("parking:info", parkingInfo);
         console.log("Request received!");
         res.sendStatus(200);
 });
