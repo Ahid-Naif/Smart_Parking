082a42a607b3ede82b9f77527849850a90f9519f
unknown <ahedqaood19@gmail.com>
2019-11-23 04:37:29 +0300

    reorganize code

--- routes.js
+++ routes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 
-router.post('/parking_lot_status', (req, res) => {
+router.post('/parking1_lot_status', (req, res) => {
         let io = req.app.get('socketio');
         let parkingInfo = req.body;
         
-        io.emit("parking:info", parkingInfo);
+        io.emit("parking1:info", parkingInfo);
+        console.log("Request received!");
+        res.sendStatus(200);
+});
+
+router.post('/parking2_lot_status', (req, res) => {
+        let io = req.app.get('socketio');
+        let parkingInfo = req.body;
+        
+        io.emit("parking2:info", parkingInfo);
         console.log("Request received!");
         res.sendStatus(200);
 });
